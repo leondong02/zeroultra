d24b5319af48e83eece45332f2cdbb4ac9dac5b9
Leon Dong <leon.d1043@gmail.com>
2025-09-06 14:07:09 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -223,14 +223,13 @@ export default function Home() {
             <div class="text-3xl font-bold my-1.5 md:my-2.5"><span class="highlight">leon dong</span></div>
             <div>
               <span class="highlight">
-                i&apos;m a 4th year computer science student with a minor in combinatorics and optimization @&nbsp;
-                <a href='https://uwaterloo.ca/' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-600 hover:text-violet-700 duration-100">uwaterloo</a>
+                i studied at the <a href='https://uwaterloo.ca/' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-600 hover:text-violet-700 duration-100">university of waterloo</a> where i got a bachelor&apos;s of computer science with a minor in combinatorics and optimization @&nbsp;
               </span>
             </div>
             <br></br>
             <div>
               <span class="highlight">
-                incoming swe @ <a href='https://www.meta.com/' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-600 hover:text-violet-700 duration-100">meta</a>
+                currently swe @ <a href='https://www.meta.com/' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-600 hover:text-violet-700 duration-100">meta</a>, instagram genai creation where i work on ml for creators
               </span>
               <br></br>
               <span class="highlight">
